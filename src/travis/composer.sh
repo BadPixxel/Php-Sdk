81626e019973050ab3e46994ba6a9111dd094153
BadPixxel <eshop.bpaquier@gmail.com>
2020-11-09 01:51:54 +0100

    Initial Commit

---
+++ src/travis/composer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+################################################################################
+#
+# Copyright (C) 2020 BadPixxel <www.badpixxel.com>
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+################################################################################
+
+echo -e "\e[104m Install Composer Dependencies                        \e[49m"
+
+echo "Composer => Update"
+composer update  --prefer-dist --no-interaction  
