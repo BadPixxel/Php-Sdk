475af0c3785ce5d209d94c808142e64ac3014063
BadPixxel <eshop.bpaquier@gmail.com>
2020-11-09 12:20:48 +0100

    Initial Commit

---
+++ src/grumphp/src/Configuration/GrumPHPExtension.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ *  Copyright (C) 2020 BadPixxel <www.badpixxel.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace GrumPHP\Configuration;
+
+use BadPixxel\PhpSdk\Configuration\Configuration;
+use GrumPHP\Exception\DeprecatedException;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class GrumPHPExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $this->loadInternal(
+            $this->processConfiguration(
+                $this->getConfiguration($configs, $container),
+                $configs
+            ),
+            $container
+        );
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
+    {
+        return new Configuration();
+    }
+
+    public function getAlias()
+    {
+        return 'grumphp';
+    }
+
+    private function loadInternal(array $config, ContainerBuilder $container): void
+    {
+        foreach ($config as $key => $value) {
+            // We require to use grumphp instead of parameters at this point:
+            if ($container->hasParameter($key)) {
+                throw DeprecatedException::directParameterConfiguration($key);
+            }
+
+            $container->setParameter($key, $value);
+        }
+    }
+}
