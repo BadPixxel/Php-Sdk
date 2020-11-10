f22f3eeb6016105f612badcd7596f41108d9a36b
BadPixxel <eshop.bpaquier@gmail.com>
2020-11-10 15:11:33 +0100

    WIP: Gitlab CI

--- docker/update.sh
+++ docker/update.sh
@@ -1,7 +1,29 @@
-########################################################
-# Connect Docker to GitLab
-docker login https://registry.gitlab.com -u BadPixxel
-########################################################
-# [PHP-7.3] Build & Upload Splash Docker Image
+
+################################################################################
+#
+# Copyright (C) 2020 BadPixxel <www.badpixxel.com>
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+################################################################################
+
+echo "[PHP 7.2] Build & Upload Docker Image"
+docker build -t registry.gitlab.com/badpixxel/php-sdk:php-7.2 docker/php-7.2
+docker push registry.gitlab.com/badpixxel/php-sdk:php-7.2
+
+echo "[PHP 7.3] Build & Upload Docker Image"
 docker build -t registry.gitlab.com/badpixxel/php-sdk:php-7.3 docker/php-7.3
 docker push registry.gitlab.com/badpixxel/php-sdk:php-7.3
+
+echo "[PHP 7.4] Build & Upload Docker Image"
+docker build -t registry.gitlab.com/badpixxel/php-sdk:php-7.4 docker/php-7.4
+docker push registry.gitlab.com/badpixxel/php-sdk:php-7.4
+
+echo "[PHP 8.0] Build & Upload Docker Image"
+docker build -t registry.gitlab.com/badpixxel/php-sdk:php-8.0 docker/php-8.0
+docker push registry.gitlab.com/badpixxel/php-sdk:php-8.0
