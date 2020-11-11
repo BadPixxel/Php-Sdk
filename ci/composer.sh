899f8f0c6607b80e4a28240ab4a4c272ff56e9a9
badpixxel <eshop.bpaquier@gmail.com>
2020-11-11 21:58:14 +0100

    WIP

--- ci/composer.sh
+++ ci/composer.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 #
 # Copyright (C) 2020 BadPixxel <www.badpixxel.com>
@@ -16,5 +16,16 @@ echo "----------------------------------------------------"
 echo "--> Install Composer Dependencies"
 echo "----------------------------------------------------"
 
+################################################################################
+# Verify Composer is Installed
+if ! command -v composer &> /dev/null
+then
+  echo "Composer => Installation Required"
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  php -r "unlink('composer-setup.php');"
+fi
+
+
 echo "Composer => Update"
-composer update  --prefer-dist --no-interaction --no-progress --no-suggest
+composer update  --prefer-dist --no-interaction --no-progress
