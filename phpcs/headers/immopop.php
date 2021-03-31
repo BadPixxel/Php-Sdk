3c2dbc83cd2b9694fa3cbd3358b6e9db7e1cff36
BadPixxel <eshop.bpaquier@gmail.com>
2021-03-31 13:41:24 +0200

    ADD: unzip

---
+++ phpcs/headers/immopop.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ *  Copyright (C) 2021 BadPixxel <www.badpixxel.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+global $header;
+
+$header = <<<'EOF'
+This file is part of Immo-Pop Website Project.
+
+Copyright (C) Immo-Pop SAS <www.immo-pop.com>
+All rights reserved.
+
+NOTE: All information contained herein is, and remains the property of Splash 
+Sync and its suppliers, if any.  The intellectual and technical concepts 
+contained herein are proprietary to Splash Sync and its suppliers, and are 
+protected by trade secret or copyright law. Dissemination of this information
+or reproduction of this material is strictly forbidden unless prior written 
+permission is obtained from Splash Sync.
+
+@author Bernard Paquier <contact@splashsync.com>
+@author Yohann Bernard <contact@immo-pop.com>
+EOF;
