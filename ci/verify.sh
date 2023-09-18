89768b2afcc9becd3193cfae1b547db0feb3bb85
BadPixxel <eshop.bpaquier@gmail.com>
2023-09-18 22:35:40 +0200

    Remove XmlLint => Deprecated

--- ci/verify.sh
+++ ci/verify.sh
@@ -16,55 +16,26 @@
 #
 ################################################################################
 
+set -e
+
+################################################################################
+# Docker Compose Container you want to check
+CONTAINERS="php-8.2,php-8.1,php-8.0,php-7.4,php-7.3,php-7.2"
+
 ################################################################################
 # Start Docker Compose Stack
 echo '===> Start Docker Stack'
 docker-compose up -d
 
-################################################################################
-# PHP 7.2
-echo '===> Checks Php 7.2'
-docker-compose exec php-7.2 composer update -q
-docker-compose exec php-7.2 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-7.2 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-7.2 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 7.3
-echo '===> Checks Php 7.3'
-docker-compose exec php-7.3 composer update -q
-docker-compose exec php-7.3 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-7.3 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-7.3 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 7.4
-echo '===> Checks Php 7.4'
-docker-compose exec php-7.4 composer update -q
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-7.4 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 8.0
-echo '===> Checks Php 8.0'
-docker-compose exec php-8.0 composer update -q
-docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-8.0 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 8.1
-echo '===> Checks Php 8.1'
-docker-compose exec php-8.1 composer update -q
-docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-8.1 php vendor/bin/grumphp run --testsuite=phpstan
-
-################################################################################
-# PHP 8.2
-echo '===> Checks Php 8.2'
-docker-compose exec php-8.2 composer update -q
-docker-compose exec php-8.2 php vendor/bin/grumphp run --testsuite=travis
-docker-compose exec php-8.2 php vendor/bin/grumphp run --testsuite=csfixer
-docker-compose exec php-8.2 php vendor/bin/grumphp run --testsuite=phpstan
+######################################
+# Walk on Docker Compose Container
+for ID in $(echo $CONTAINERS | tr "," "\n")
+do
+    echo "----------------------------------------------------"
+    echo "===> CHECKS ON $ID"
+    echo "----------------------------------------------------"
+    docker-compose exec $ID composer update -q || docker-compose exec $ID composer update
+    docker-compose exec $ID php vendor/bin/grumphp run --testsuite=travis
+    docker-compose exec $ID php vendor/bin/grumphp run --testsuite=csfixer
+    docker-compose exec $ID php vendor/bin/grumphp run --testsuite=phpstan
+done
