558cd61e62c79165d658877ad9c0a88c66e84de7
BadPixxel <eshop.bpaquier@gmail.com>
2025-08-11 15:11:41 +0200

    Streamline Docker build script by replacing separate `docker build` and `docker push` commands with integrated `--push` flag for efficiency.

--- docker/update.sh
+++ docker/update.sh
@@ -13,20 +13,16 @@
 ################################################################################
 
 echo "[PHP 7.2] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.2 docker/php-7.2
-docker push registry.gitlab.com/badpixxel-projects/php-sdk:php-7.2
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.2 docker/php-7.2                   --push
 
 echo "[PHP 7.3] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.3 docker/php-7.3
-docker push registry.gitlab.com/badpixxel-projects/php-sdk:php-7.3
+docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.3 docker/php-7.3                   --push
 
 echo "[PHP 7.4] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.4 docker/php-7.4
-docker push registry.gitlab.com/badpixxel-projects/php-sdk:php-7.4
+docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.4 docker/php-7.4                   --push
 
 echo "[PHP 8.0] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.0 docker/php-8.0
-docker push registry.gitlab.com/badpixxel-projects/php-sdk:php-8.0
+docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.0 docker/php-8.0                   --push
 
 echo "[PHP 8.1] Build & Upload Docker Image"
 docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.1          docker/php-8.1          --push
