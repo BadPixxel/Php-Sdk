f9e63951ab92088344e586d42a62d948ffd1a6b0
BadPixxel <eshop.bpaquier@gmail.com>
2025-08-11 15:40:10 +0200

    Update Debian sources in PHP 7.2 and 7.4 Dockerfiles to use archive repositories. Replace `docker build` with `docker buildx` in the build script for consistency and multi-platform support.

--- docker/update.sh
+++ docker/update.sh
@@ -16,31 +16,31 @@ echo "[PHP 7.2] Build & Upload Docker Image"
 docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.2 docker/php-7.2                   --push
 
 echo "[PHP 7.3] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.3 docker/php-7.3                   --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.3 docker/php-7.3                   --push
 
 echo "[PHP 7.4] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.4 docker/php-7.4                   --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-7.4 docker/php-7.4                   --push
 
 echo "[PHP 8.0] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.0 docker/php-8.0                   --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.0 docker/php-8.0                   --push
 
 echo "[PHP 8.1] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.1          docker/php-8.1          --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.1          docker/php-8.1          --push
 
 echo "[PHP 8.2] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.2          docker/php-8.2          --push
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.2-apache   docker/php-8.2-apache   --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.2          docker/php-8.2          --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.2-apache   docker/php-8.2-apache   --push
 
 echo "[PHP 8.3] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.3          docker/php-8.3          --push
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.3-apache   docker/php-8.3-apache   --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.3          docker/php-8.3          --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.3-apache   docker/php-8.3-apache   --push
 
 echo "[PHP 8.4] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.4          docker/php-8.4          --push
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.4-apache   docker/php-8.4-apache   --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.4          docker/php-8.4          --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:php-8.4-apache   docker/php-8.4-apache   --push
 
 echo "[JEKYLL] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:jekyll docker/jekyll                     --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:jekyll docker/jekyll                     --push
 
 echo "[JEKYLL PHP 7.4] Build & Upload Docker Image"
-docker build -t registry.gitlab.com/badpixxel-projects/php-sdk:jekyll-7.4 docker/jekyll-7.4             --push
+docker buildx build -t registry.gitlab.com/badpixxel-projects/php-sdk:jekyll-7.4 docker/jekyll-7.4             --push
